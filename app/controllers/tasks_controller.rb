4b66ed665d2562cb5cfebf3015fb69c2978df090
rika kaminoyama <richun.06@gmail.com>
2022-12-29 01:04:23 +0900

    タスク登録機能の実装、リンクの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,23 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを登録しました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
