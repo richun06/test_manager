e565436e47186cfae9f657ab83c7472fea1761f2
rika kaminoyama <richun.06@gmail.com>
2022-12-29 01:53:22 +0900

    UserとTaksの関連付けのため外部キーを設定しアソシエーションを定義する

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 20 }
   validates :content, presence: true, length: { maximum: 300 }
+
+  belongs_to :user
 end
