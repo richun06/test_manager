03e41d7bbc640e99c92427fdd6bb4427346ec406
rika kaminoyama <richun.06@gmail.com>
2022-12-29 03:25:42 +0900

    ラベル登録、一覧機能の実装、リンクの追加

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,2 +1,26 @@
 class LabelsController < ApplicationController
+  def index
+    @labels = Label.where(user_id: current_user.id)
+  end
+
+  def new
+    @label = Label.new
+  end
+
+  def create
+    @label = Label.new(label_params)
+    @label.user_id = current_user.id
+    if @label.save
+      redirect_to labels_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def label_params
+    params.require(:label).permit(:name, :user_id)
+  end
+
 end
