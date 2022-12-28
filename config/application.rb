06ee4d95daee3762f27a6c86a5601436b685635d
rika kaminoyama <richun.06@gmail.com>
2022-12-29 00:55:49 +0900

    タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module ManagerApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
