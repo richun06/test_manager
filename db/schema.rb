24d3ea01c22e6bb9f435953283f2b78d40c4f0a1
rika kaminoyama <richun.06@gmail.com>
2022-12-29 02:32:30 +0900

    UserとLabelを関連づけるため外部キーの設定をしアソシエーションを定義する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_28_171419) do
+ActiveRecord::Schema.define(version: 2022_12_28_173013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2022_12_28_171419) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_labels_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -59,5 +61,6 @@ ActiveRecord::Schema.define(version: 2022_12_28_171419) do
 
   add_foreign_key "labellings", "labels"
   add_foreign_key "labellings", "tasks"
+  add_foreign_key "labels", "users"
   add_foreign_key "tasks", "users"
 end
