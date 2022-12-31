d28a01405816f7ad190b97073916789cb89fd669
rika kaminoyama <richun.06@gmail.com>
2022-12-31 17:10:17 +0900

    中間テーブルを削除した際に消し忘れたアソシエーションを削除

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,3 @@
 class Label < ApplicationRecord
-  has_many :labellings, dependent: :destroy
-  has_many :tasks, through: :labellings
   belongs_to :user
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,4 @@ class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 300 }
 
   belongs_to :user
-  has_many :labellings, dependent: :destroy
-  has_many :labels, through: :labellings
 end
